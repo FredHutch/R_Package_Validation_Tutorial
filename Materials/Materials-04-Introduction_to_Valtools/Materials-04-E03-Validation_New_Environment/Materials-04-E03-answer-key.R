9f1c4490b94e7e9d297fe6f06a87c7b2b266d037
Marie Vendettuoli <mvendett@scharp.org>
2021-05-27 19:14:50 -0700

    validation modes

---
+++ Materials/Materials-04-Introduction_to_Valtools/Materials-04-E03-Validation_New_Environment/Materials-04-E03-answer-key.R
@@ -0,0 +1,27 @@
+# setup clone RPackageValidationTutorial.04E03
+
+# Discussion: What are the different models of validation?
+#    Already covered: 
+#      - E01: validate outside of installed package aka "third-party validation"
+#      - E02: validate from code aka "validate while developing"
+
+# 
+
+########### Task 04_E03 B
+# validate on install
+vt_validate_install()
+
+########### Task 04_E03 C
+# build a validated package to share
+vt_validate_build()
+
+########## Task 04_E03 D
+# re-validate a package that has been installed
+vt_validate_installed_package()
+
+
+# Discussion: 
+# 1. When do you see each mode being useful?
+# 2. What documentation will be helpful to share with stakeholders?
+
+
