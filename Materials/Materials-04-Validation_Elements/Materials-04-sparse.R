c70ea7023452bc8831d0c05e0eb9793bcc6372f3
thebioengineer <ellishughes@live.com>
2021-09-10 15:13:34 -0700

    Update slides/exercise 4

--- Materials/Materials-04-Validation_Elements/Materials-04-sparse.R
+++ Materials/Materials-04-Validation_Elements/Materials-04-sparse.R
@@ -1,32 +1,39 @@
 ########### setup
+
+# We have been a partially completed validation packet. Lets complete it. 
+
 library(valtools)
-setwd(here::here("Materials/Materials-04-Introduction_to_Valtools/Materials-04-E01-Validation_Elements/simple_validation"))
+setwd(here::here("Materials/Materials-04-Validation_Elements/simple_validation"))
 
 
-########### Task 04_E01 A
-# render the validation report found in folder "simple_validation"
+########### Task 04 A
 
-########### Task 04_E01 B
-# Add missing case 001 to check valtools version is >= 0.3.0
+# Add another requirement that valtools version is >= 0.3.0
+# Add another Test Case to test that valtools version is >= 0.3.0
+# Add another Test Case to prove that valtools version is >= 0.3.0
 
 # Requirement details -- 
-# Activity: update the requirement and assessment with `1.1:1, High Risk, High Impact`. 
+# Activity: respond to prompt for username.
+#        Y to add user to config
+#        "name" and "title" are free response
+#        "role" should be "Validation Lead"
+# Activity: Replace  `REQUIREMENT: ASSESSMENT` with `1.1:1, High Risk, High Impact`. 
 # Activity: copy the following content - 
-#   ## 001. `valtools` version installed {-}
+#   ## 002. `valtools` version installed {-}
 # 
-#   *Requirement*
 #   
-#     + `{valtools}` is the version approved for usage.
+#     + `2.1 {valtools}` is the version approved for usage.
 
 # Test Case details -- 
-# Activity: update the coverage to `1.1: 1.1`. 
+# Activity: replace `TESTCASE: REQUIREMENT` with `2.1: 2.1`. 
+# Discussion: How would this change with additional test cases or requirements?
 # Activity: copy the following content - 
 #   *Test Cases*
 #   
-#     + Test that the version of `{valtools}` is `>= 0.3.0`
+#     + 2.1 Test that the version of `{valtools}` is `>= 0.3.0`
 
 # Test Code details --
-# Activity: Update `TESTNUMBER` to `T1.1` and copy the following into the body of the test - 
+# Activity: Update `TESTNUMBER` to `2.1` and copy the following into the body of the test - 
 #   expect_true(
 #     packageVersion("valtools") >= "0.3.0"
 #   )
@@ -36,18 +43,13 @@ setwd(here::here("Materials/Materials-04-Introduction_to_Valtools/Materials-04-E
 # Discussion: How does TESTCASE:REQUIREMENT change with additional test cases or requirements?
 # Discussion: Why should we add a different person to the @editor field for test code?
 
-########### Task 04_E01 C
-# render the updated validation report
-# Discussion: What looks odd here? Is the "Validation" section ordered as expected?
 
-########### Task 04_E01 D
-# drop case 002 files behind case 001 in the validation section 
+########### Task 04 B
+# render the validation report found in folder "simple_validation"
+
+
+########### Task 04 C
+# Move Test Case 002 files ahead of Test case 001 in the validation section 
 # Discussion: Where is the child file ordering info stored? 
 # Discussion: Is the validation_files field required? Hint `?vt_get_child_files()`
-
-########### Task 04_E01 E
-# switch to using dynamic rendering for requirement 001
-# Discussion: look at validation.Rmd - how do we initiate dynamic referencing?
-# Bonus1: add dynamic referencing elsewhere in validation report
-# Bonus2: switch from ## flags to a different flag. Do some work better than others?
 
